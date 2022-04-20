b4de9a4e82c829380cc9be9701e89e3120634904
felipev3d <felipe@v2fineinteriors.com>
2022-04-20 06:50:04 -0700

    materials Labels

--- index.js
+++ index.js
@@ -1,48 +1,154 @@
 const express = require('express')
 const escpos = require('escpos');
+const cors = require('cors');
 escpos.USB = require('escpos-usb');
-const device  = new escpos.USB();
 
-const options = { encoding: "GB18030" /* default */ }
-const printer = new escpos.Printer(device, options);
 
 const app = express()
 const port = 5000;
+app.use(cors(
+  { origin: "*" }
+))
 
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// const deviceName = escpos.USB.findPrinter()
+// console.log(deviceName);
+
+
 
 app.post('/', (req, res) => {
-    const data = req.body;
-    console.log(data)
+  const arr1 = req.body;
+  console.log(arr1)
 
-    device.open(function(error){
-        printer
-        .font('a')
-        .align('ct')
-        .style('bu')
-        .size(1, 1)
-        .text('The quick brown fox jumps over the lazy dog')
-        .text('Prueba')
-        .barcode('1234567', 'EAN8')
-        .table(["One", "Two", "Three"])
-        .tableCustom(
-          [
-            { text:"Left", align:"LEFT", width:0.33, style: 'B' },
-            { text:"Center", align:"CENTER", width:0.33},
-            { text:"Right", align:"RIGHT", width:0.33 }
-          ],
-          { encoding: 'cp857', size: [1, 1] } // Optional
-        )
-        .qrimage('https://github.com/song940/node-escpos', function(err){
-          this.cut();
-          this.close();
+  arr1.forEach(async data => {
+
+
+    (async () => {
+      const device = await new escpos.USB(1208, 3624); const options = { encoding: "GB18030" /* default */ }
+      const printer = await new escpos.Printer(device, options);
+      (() => {
+        device.open(async function (error) {
+          console.log(error);
+          if (true) {
+            await printer
+              .font('a')
+              .align('lt')
+              .style('bu')
+              .size(0, 0)
+              .text("Material:")
+              .align('ct')
+              .text(data.material)
+              .align('lt')
+              .text("Vendor:")
+              .align('ct')
+              .text(data.vendor)
+              .align('ct')
+              .size(0, 1)
+              .tableCustom(
+                [
+                  { text: "Quantity", align: "LEFT", width: 0.33, style: 'B' },
+                  { text: data.qty, align: "CENTER", width: 0.33 }
+                ],
+                { encoding: 'cp857', size: [1, 1] } // Optional
+              )
+              .tableCustom(
+                [
+                  { text: "Received", align: "LEFT", width: 0.33, style: 'B' },
+                  { text: new Date().toISOString().slice(0, 10), align: "CENTER", width: 0.33 }
+                ],
+                { encoding: 'cp857', size: [1, 1] } // Optional
+              )
+
+              .align('ct')
+              .size(0, 0)
+              .text('Project')
+              .size(1, 1)
+              .text(data.project.name)
+              .size(0, 1)
+              .text(data.project.area)
+              .tableCustom(
+                [
+                  { text: "Mat ID", align: "LEFT", width: 0.33, style: 'B' },
+                  { text: data.project.matId, align: "CENTER", width: 0.33 }
+                ],
+                // { encoding: 'cp857', size: [1, 1] } // Optional
+              )
+              .qrimage(data.inventoryID, function (err) {
+
+                printer.cut()
+                printer.close()
+
+              });
+
+          } else {
+
+            await printer
+              .font('a')
+              .align('lt')
+              .style('bu')
+              .size(0, 0)
+              .text("Material:")
+              .align('ct')
+              .text(data.material)
+              .align('lt')
+              .text("Vendor:")
+              .align('ct')
+              .text(data.vendor)
+              .align('ct')
+              .size(0, 1)
+              .tableCustom(
+                [
+                  { text: "Quantity", align: "LEFT", width: 0.33, style: 'B' },
+                  { text: data.qty, align: "CENTER", width: 0.33 }
+                ],
+                { encoding: 'cp857', size: [1, 1] } // Optional
+              )
+              .tableCustom(
+                [
+                  { text: "Received", align: "LEFT", width: 0.33, style: 'B' },
+                  { text: new Date().toISOString().slice(0, 10), align: "CENTER", width: 0.33 }
+                ],
+                { encoding: 'cp857', size: [1, 1] } // Optional
+              )
+              .tableCustom(
+                [
+                  { text: "Mat ID", align: "LEFT", width: 0.33, style: 'B' },
+                  { text: data.project.matId, align: "CENTER", width: 0.33 }
+                ],
+                // { encoding: 'cp857', size: [1, 1] } // Optional
+              )
+              .qrimage(data.inventoryID, function (err) {
+
+                // this.close();
+                // this.close();
+                // resolve()
+                printer.cut()
+                printer.close()
+              });
+
+
+
+
+          }
         });
-      });
 
-  res.send('Hello World!')
+
+      })()
+      device.open(async function (error) {
+
+        // await printJob(alldata)
+
+      })
+    })();
+  });
+  res.send("he")
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
